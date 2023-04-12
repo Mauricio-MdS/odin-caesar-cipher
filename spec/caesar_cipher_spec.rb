09bc6b85db398fed9542052e67951037baa81cd0
Mauricio-MdS <45921381+Mauricio-MdS@users.noreply.github.com>
2023-04-12 14:11:43 -0300

    test text with special characters

--- spec/caesar_cipher_spec.rb
+++ spec/caesar_cipher_spec.rb
@@ -7,5 +7,9 @@ describe 'caesar_chiper' do
     it 'returns a word with characters shifted' do
       expect(cipher_text('hello', 2)).to eql('jgnnq')
     end
+
+    it 'returns a text with special characterers unshifted, but letters shifted' do
+      expect(cipher_text('hello, world!', 2)).to eql('jgnnq, yqtnf!')
+    end
   end
 end
